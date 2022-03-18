063cecef1edc60f491e2bdb70a39339c5f8e9290
Anusha <anushatr11@gmail.com>
2022-03-18 17:01:05 +0530

    Linked List

--- main.c
+++ main.c
@@ -1,7 +1,42 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+	int val;
+	struct node *next;
+};
+
+typedef struct node node_t;
+
+void printList(node_t *head) {
+	node_t *tmp = head;
+	while (tmp != NULL) {
+		printf("- %d -", tmp->val);
+		tmp = tmp->next;
+	}
+	printf("\n");
+}
+
+void insertAtBeg(node_t *head, int val) {
+	node_t *tmp = head;
+	tmp->val = val;
+	if (tmp == NULL) {
+		tmp->next = NULL;
+	} else {
+		tmp->next = head;
+	}
+	head = tmp;
+}
+
 
 int main(){
-	printf("Hello World");
+	node_t *head = NULL;
+	
+	for (int i = 0; i < 10; i++) {
+		insertAtBeg(head, i);
+	}
+
+	printList(head);
 
 	return 0;
 }
